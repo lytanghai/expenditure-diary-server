6b7b89d74e53d2501c5010e2a2677c798d3a5cd8
Tanghai Ly <tanghai.ly@wingbank.com.kh>
2025-09-05 16:46:58 +0700

    full apply

--- src/main/java/com/expenditure_diary/expenditure_diary/util/JwtUtil.java
+++ src/main/java/com/expenditure_diary/expenditure_diary/util/JwtUtil.java
@@ -3,25 +3,21 @@ package com.expenditure_diary.expenditure_diary.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.SecretKey;
-import java.util.Base64;
 
 @Component
 public class JwtUtil {
 
     private final long jwtExpirationMs = 3600000;
 
-    private final SecretKey key = Keys.hmacShaKeyFor(
-        Base64.getDecoder().decode("yI5uXwrEt9pR7hcgV8nE0Wp3K1e8VsE6n9t7WRrN6uGtdjGQYq+6sZGc1K3qC3phFVzxEWExyqAHaVeryLongSecretKeyThatIsAtLeastSixtyFourCharactersLongForHS512Encryption")
-    );
+    @Value("${jwt.secret_key}")
+    private String secret;
 
     public String validateAndGetUsername(String token) {
         try {
             Claims claims = Jwts.parser()
-                    .setSigningKey(key) // must match the signing secret
+                    .setSigningKey(secret) // must match the signing secret
                     .parseClaimsJws(token)
                     .getBody();
 
