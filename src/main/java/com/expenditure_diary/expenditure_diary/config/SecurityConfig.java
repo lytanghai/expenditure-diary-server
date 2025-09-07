3b80d64f26c229ad3c515d458ac03c5923159492
unknown <lytanghaii@gmail.com>
2025-09-07 17:05:24 +0700

    disable cors

--- src/main/java/com/expenditure_diary/expenditure_diary/config/SecurityConfig.java
+++ src/main/java/com/expenditure_diary/expenditure_diary/config/SecurityConfig.java
@@ -2,24 +2,59 @@ package com.expenditure_diary.expenditure_diary.config;
 
 import com.expenditure_diary.expenditure_diary.component.JwtAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${backend_server.web_url}")
+    private String SERVER_URL;
 
     @Autowired
     private JwtAuthFilter jwtAuthFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+            .csrf().disable()
+            .cors().configurationSource(corsConfigurationSource()) // enable CORS
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .authorizeRequests()
             .antMatchers("/public/**").permitAll()
+            .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
             .anyRequest().authenticated();
 
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        System.out.println(SERVER_URL);
+        config.setAllowedOrigins(List.of(
+                "https://expense-tracker-v2-web.onrender.com",
+                "http://localhost:5173"// frontend
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
